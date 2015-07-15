865c8c9965ea228ebfd91d60d7eeeb758d445035
Bruna Lima <bru.allima@gmail.com>
2015-07-14 23:44:37 -0300

    Adicionando assunto por nome formspree

---
+++ js/files/formspree.js
@@ -0,0 +1,10 @@
+(function formspree() {
+	var doc = document;
+	var getElement = function (id) {
+		return doc.getElementById(id);
+	};
+
+	getElement("name-contact-form").addEventListener("change", function() {
+		getElement("subject-contact-form").setAttribute("value", "Contato Portfólio - " + this.value);
+	});
+})();
